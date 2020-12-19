814b86afe01390e6f1232d5458d528939e6523d5
flareair <flareair0@gmail.com>
2020-12-20 00:48:50 +0300

    ADd async validations

--- async.ts
+++ async.ts
@@ -3,23 +3,43 @@ import { pipe } from 'fp-ts/function'
 import * as TE from 'fp-ts/TaskEither'
 import * as T from 'fp-ts/Task'
 import * as E from 'fp-ts/Either'
+import * as t from 'io-ts'
+import { formatValidationErrors } from 'io-ts-reporters'
 
-const getData = (url: string) => pipe(
+const ApiResponse = t.type({
+    data: t.type({
+        dates: t.type({
+            day1: t.number,
+            month: t.number
+        }),
+        namedays: t.UnknownRecord
+    })
+})
+
+type ApiResponse = t.TypeOf<typeof ApiResponse>
+
+const getData = <T>(url: string) => pipe(
     TE.tryCatch(
-        () => axios.get(url),
+        () => axios.get<T>(url),
         E.toError
     )
 )
 
-const getApiData = (url: string) => pipe(
-    getData(url),
+const getApiData = <T>(url: string) => pipe(
+    getData<T>(url),
     TE.map(({ data }) => data),
-    TE.fold(
-        err => T.of(`Error happened! ${err.message}`),
-        data => T.of(data)
-    )
 )
 
+const decodeResponse = <T>(decoder: t.Type<T>) => (data: unknown): TE.TaskEither<Error, T> => {
+    return pipe(
+        TE.fromEither(decoder.decode(data)),
+        TE.mapLeft(errors => new Error(formatValidationErrors(errors).join('/n'))),
+    )
+}
 
-getApiData('https://api.abalin.net/today?country=us&timezone=US%2FHawaii')().then(console.log)
-getApiData('https://api123.abalin.net/today?country=us&timezone=US%2FHawaii')().then(console.log)
+const fetchAndDecode = pipe(
+    getApiData<ApiResponse>('https://api.abalin.net/today?country=us&timezone=US%2FHawaii'),
+    TE.chain(data => decodeResponse<ApiResponse>(ApiResponse)(data)),
+)
+
+fetchAndDecode().then(E.fold(console.error, console.log))
